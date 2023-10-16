789b782e43baac1e957395e67b75a9b11bf2d584
olisajioke <chijioke914@gmail.com>
2023-10-15 17:58:16 -0700

    updating task 17

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,82 +1,43 @@
+import os
+import sys
 import unittest
-from unittest.mock import patch
-from io import StringIO
 from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
 
 
 class TestHBNBCommandMethods(unittest.TestCase):
-    def setUp(self):
-        self.console = HBNBCommand()
-
-    def tearDown(self):
-        self.console = None
-
-    def test_do_EOF(self):
-        """Test EOF command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.assertTrue(self.console.do_EOF(""))
-            self.assertEqual("", output.getvalue())
-
-    def test_do_quit(self):
-        """Test quit command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.assertTrue(self.console.do_quit(""))
-            self.assertEqual("", output.getvalue())
-
-    def test_emptyline(self):
-        """Test emptyline method."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.emptyline()
-            self.assertEqual("", output.getvalue())
-
     def test_do_create(self):
-        """Test create command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_create("BaseModel")
-            created_output = output.getvalue()
+        """Test the 'create' command in the console."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("create BaseModel")
+            created_output = output.getvalue().strip()
             self.assertIn("created", created_output)
-            self.assertIn("BaseModel", created_output)
 
     def test_do_show(self):
-        """Test show command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_show("BaseModel 1234-5678")
-            shown_output = output.getvalue()
+        """Test the 'show' command in the console."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("show BaseModel")
+            shown_output = output.getvalue().strip()
             self.assertIn("BaseModel", shown_output)
-            self.assertIn("1234-5678", shown_output)
 
     def test_do_destroy(self):
-        """Test destroy command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_destroy("BaseModel 1234-5678")
-            destroyed_output = output.getvalue()
+        """Test the 'destroy' command in the console."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("destroy BaseModel")
+            destroyed_output = output.getvalue().strip()
             self.assertIn("BaseModel", destroyed_output)
-            self.assertIn("1234-5678", destroyed_output)
-
-    def test_do_all(self):
-        """Test all command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_all("BaseModel")
-            all_output = output.getvalue()
-            self.assertIn("BaseModel", all_output)
 
     def test_do_update(self):
-        """Test update command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_update("BaseModel 1234-5678 attribute value")
-            update_output = output.getvalue()
+        """Test the 'update' command in the console."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("update BaseModel 1 name 'NewName'")
+            update_output = output.getvalue().strip()
             self.assertIn("BaseModel", update_output)
-            self.assertIn("1234-5678", update_output)
-            self.assertIn("attribute", update_output)
-            self.assertIn("value", update_output)
 
     def test_do_count(self):
-        """Test count command."""
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.do_count("BaseModel")
-            count_output = output.getvalue()
-            self.assertIn("BaseModel", count_output)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        """Test the 'count' command in the console."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("count BaseModel")
+            count_output = output.getvalue().strip()
+            self.assertEqual(count_output, '1')
